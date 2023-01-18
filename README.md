a30875ab6fc4ac40d29bbb7214a4fd12b69dadc3
hamkasatria <muhhamkasatrianto@gmail.com>
2023-01-18 15:57:09 +0700

    add implementation interface

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# sibi_detection
-sibi detection with faster rcnn
+# SIBI- Detection
+`creator : Hamka Satria`
+<br><br>
+
+## Download models with command
+```
+wget https://dl.dropboxusercontent.com/s/dlww6malzd6pwtj/FRCNN-R18.pth
+
+wget https://dl.dropboxusercontent.com/s/y3jmxt69p9uswnc/FRCNN-R50.pth
+```
+
+`Note`
+
+change the models type with change the `selection` in app.py files
+
+
+<br><br>
+
+## A. Setup Local
+<br>
+
+### 1. Install IceVision
+
+```
+pip install icevision[all]
+```
+
+### 2. Install Gradio
+```
+pip install gradio
+```
+### 3. run app
+```
+python3 app.py
+```
+
+<br>
+
+
+## B. Run With Docker
+<br>
+
+To build the icevision image, run the following command:
+```
+docker-compose build
+```
+
+Once the icevision image has been successfully built, run the icevision service:
+```
+ docker-compose run --rm -d icevision
+```
+
+**Note**
+For more detailed instructions on how to use this icevision docker container as well how connect it to Visual Studio Code, check out this in-depth [blog post](https://francescopochetti.com/developing-inside-a-docker-container-in-visual-studio-code/).
---
+++ app.py
@@ -0,0 +1,115 @@
+from icevision.all import *
+
+import PIL
+import torch
+import gradio as gr
+from  gradio.components import *
+import time
+
+
+# Declare
+class_map = ClassMap(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+image_size = 384
+valid_tfms = tfms.A.Adapter([*tfms.A.resize_and_pad(image_size), tfms.A.Normalize()])
+
+
+def init_models(model_type, backbone):
+  model = model_type.model(backbone=backbone(pretrained=True), num_classes=len(class_map)) 
+  return model, model_type
+
+def set_models(mdl, path_best_model, model_name):
+  class LightModel(model_type.lightning.ModelAdapter):
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=1e-4)
+
+  metrics = [COCOMetric(metric_type=COCOMetricType.bbox)]
+  light_model = LightModel(mdl, metrics=metrics)
+  mdl.load_state_dict(torch.load(path_best_model + model_name))
+
+def load_model(backbone, model_name):
+  model, mdl_type = init_models(model_type, backbone)
+  set_models(model, path_best_model, model_name)
+  return model, mdl_type
+
+
+# Just change the value of selection to try another model
+# 0 -> faster rcnn resnet 50 
+# 1 -> faster rcnn resnet 18
+selection = 0
+path_best_model = ''
+
+extra_args = {}
+model_type = models.torchvision.faster_rcnn
+
+if selection == 0:
+  backbone = model_type.backbones.resnet_fpn_configs.resnet50_fpn
+  model_name = 'FRCNN-R50.pth'
+  
+if selection == 1:
+  backbone = model_type.backbones.resnet_fpn_configs.resnet18_fpn
+  model_name = 'FRCNN-R18.pth'
+
+
+  
+model, model_type = load_model(backbone, model_name)
+
+
+start_time = ""
+count = 0
+def pred(input_image):
+  global count, start_time, model_name
+  if start_time == "":
+    start_time = time.time()
+  # try:
+  detection_threshold=0.5
+  display_label = True
+  display_bbox = True
+
+  img = PIL.Image.fromarray(input_image, 'RGB')
+  pred_dict  = model_type.end2end_detect(img, valid_tfms, model, class_map=class_map, detection_threshold=detection_threshold, display_label=display_label, display_bbox=display_bbox, return_img=True, font_size=16, label_color="#FF59D6")
+  count = count +1
+  if count == 100:
+    end_time = time.time()
+    elapsed = end_time-start_time
+    str_time = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+    
+    print("Model = ", model_name)
+    print("time = ", str_time)
+    print ("FPS = ", count,"/", int(elapsed)," = ", (count/int(elapsed)) )
+
+  return pred_dict['img']
+    
+  # except:
+  #   raise gr.Error("Error file type ! ")
+    # return input_image
+
+
+# def pred(img):
+#   return img
+
+with gr.Blocks() as demo:
+    gr.Markdown('<h1> <p style="text-align: center;"> Pengenalan Abjad SIBI</p></h1>')
+    with gr.Tab("Image"):
+        with gr.Row():
+            with gr.Column():
+              image_input = gr.Image(label="Input")
+              with gr.Row():
+                image_button = gr.Button("Submit")
+            with gr.Column():
+              image_output = gr.Image(label="Output")
+    
+        
+    with gr.Tab("Webcam"):
+        with gr.Row():
+          with gr.Column():
+            image_input_cam = gr.Image(source="webcam", streaming=True)
+          with gr.Column():
+            image_output_cam = gr.Image(label="Output")
+        
+    image_input_cam.stream(fn=pred, inputs = image_input_cam, outputs = image_output_cam)
+    image_button.click(pred, inputs=image_input, outputs=image_output)
+
+
+
+
+demo.launch(inline=True, share=True, debug=True, show_error= True, server_port=7867)
